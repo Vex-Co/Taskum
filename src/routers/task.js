23e9fbef5389d89516bdfb1ca808d2da2b792ad7
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2022-04-14 05:17:12 +0500

    Clean Code.

--- src/routers/task.js
+++ src/routers/task.js
@@ -52,7 +52,6 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    // const task = await Task.findByIdAndUpdate(_id, req.body, {new:true, runValidators: true});
     const task = await Task.findById(_id);
     updates.forEach (update => {
       task[update] = req.body[update];
